8cddb38a4feaa0c8d4478fd8501e0ea65bf1bb8b
liupeng <2511634167@qq.com>
2018-08-09 10:52:28 +0800

    增加自定义函数

---
+++ app/Helpers/functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liupeng
+ * Date: 2018-08-09
+ * Time: 10:34
+ */
+
+/**
+ * 调试
+ *
+ * @param $msg
+ */
+function debug($msg)
+{
+    \ChromePhp::info($msg);
+}
